a0dd9e4d68ac3a3e97b5249fdafb2ee93377a762
Elvis Landi <elandi13@yahoo.com>
2021-01-12 13:17:55 -0500

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,19 @@
 class StudentsController < ApplicationController
+  before_action :find_student, only: :show 
+
   def index
     @students = Student.all
   end
+
+  def show
+
+  end
+
+
+  private
+
+  def find_student 
+    @student = Student.find(params[:id])
+  end
+
 end
